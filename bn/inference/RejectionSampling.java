a9aaf3f453198af97a8ca4029e811d00b8e6dd76
MrKatamari <79069849+MrKatamari@users.noreply.github.com>
2021-04-12 18:57:12 -0400

    fgh

--- bn/inference/RejectionSampling.java
+++ bn/inference/RejectionSampling.java
@@ -1,8 +1,25 @@
 package bn.inference;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import bn.core.*;
 import bn.base.Distribution;
+import bn.base.Assignment;
 
 public class RejectionSampling {
     
+    public Assignment pSample(BayesianNetwork bn)
+    {
+        Assignment tempAssignment = new Assignment();
+        List<RandomVariable> list = bn.getVariablesSortedTopologically();
+        for(RandomVariable Xi:list)
+        {
+            for(Value i:Xi.getDomain())
+            {
+                
+            }
+        }
+
+    }
 }
