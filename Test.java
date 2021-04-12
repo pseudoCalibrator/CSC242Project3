31d10b647e2bef712ea9737394051100c68a5100
Jayda Medina <43220330+jmedina7@users.noreply.github.com>
2021-04-12 15:35:25 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,27 +3,31 @@ import parser.*;
 import Inference.*;
 
 public class Test {
-    public static void main(Sting[] args){
+    public static void main(Sting[] args) {
         // If the first argument is not a number, we're running exact inferencer
-        if(!Character.isDigit(args[0].charAt(0))){
+        if (!Character.isDigit(args[0].charAt(0))) {
             ExactInference exact = new ExactInference();
             // parse xml file
-            if(args[0].endsWith("xml")){
-
+            if (args[0].endsWith("xml")) {
+                XMLBIFParser xmlparser = new XMLBIFParser();
+                // Read network from the file given
+                BayesianNetwork network = xmlparser.readNetworkFromFile(args[0]);
+                Assignment assignment = new Assignment();
             }
             // parse bif
-            else{
+            else {
 
             }
         }
-        // If the first argument is a number, we're either doing reject or weight approximate
-        else{
+        // If the first argument is a number, we're either doing reject or weight
+        // approximate
+        else {
             // parse xml file
-            if(args[0].endsWith("xml")){
+            if (args[0].endsWith("xml")) {
 
             }
             // parse bif
-            else{
+            else {
 
             }
         }
