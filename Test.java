ebe74df64bca66445f5d75ac8ef56ed6bb0fff07
Jayda Medina <43220330+jmedina7@users.noreply.github.com>
2021-04-12 01:24:17 -0400

    Update Test.java

--- Test.java
+++ Test.java
@@ -3,6 +3,9 @@ import parser.*;
 
 public class Test {
     public static void main(Sting[] args){
+        // If the first argument is a number, we're running approximate inferencers
+        if(Character.isDigit(args[0].charAt(0))){
 
+        }
     }
 }
